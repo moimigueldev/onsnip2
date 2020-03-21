7ad8bb467d9849e32cb966ba8b6feb344e63f543
moises <moimigueldev@gmail.com>
2020-03-21 10:23:00 -0600

    deleted keys;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
 
 
 
--- src/assets/keys.ts
+++
@@ -1,4 +0,0 @@
-export const spotifyKeys = {
-    'client-id': "c593349cdedd4cc5883b81baf1f76345",
-    'client-secret': '9b795b6a4e1a4953bae83db5972edef5'
-}
