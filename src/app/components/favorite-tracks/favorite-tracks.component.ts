85d0f493c9feef5b41bfadc08fe5d520cc73cf5c
moises <moimigueldev@gmail.com>
2020-03-23 17:24:24 -0600

    got toggle between dates working

--- src/app/components/favorite-tracks/favorite-tracks.component.ts
+++ src/app/components/favorite-tracks/favorite-tracks.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteTracksComponent implements OnInit {
 
+  activeClass = {
+    allTime: true,
+    sixMonths: false,
+    threeWeeks: false
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getAllTime(): void {
+    this.activeClass.allTime = true;
+    this.activeClass.sixMonths = false;
+    this.activeClass.threeWeeks = false;
+    // this.longTermSub = this.artistService.getTopArtists('long_term').subscribe(res => {
+    //   this.artists = res
+    // })
+  }
+
+  getSixMonths(): void {
+    this.activeClass.allTime = false;
+    this.activeClass.sixMonths = true;
+    this.activeClass.threeWeeks = false;
+    // this.mediumTermSub = this.artistService.getTopArtists('medium_term').subscribe(res => {
+    //   this.artists = res
+    // })
+
+  }
+  getThreeWeeks(): void {
+    this.activeClass.allTime = false;
+    this.activeClass.sixMonths = false;
+    this.activeClass.threeWeeks = true;
+    // this.shortTermSub = this.artistService.getTopArtists('short_term').subscribe(res => {
+
+    //   this.artists = res
+    // })
+
   }
 
 }
